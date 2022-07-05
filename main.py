1d2bc5d1b78e6f9d3e1f4944813341a87f237b68
nouhadonosor <nouhadonosor@gmail.com>
2022-07-05 18:21:08 +0300

    fix

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from scipy.spatial.distance import pdist
 from scipy.spatial.distance import cdist
-import operator as op
-from functools import reduce
 
 INPUT_FILENAME = 'vectors.csv'
 PLOT_X_INTERVAL = (0, 100)
-PLOT_Y_TICKS_INTERVAL = (0, 8)
+PLOT_Y_TICKS_INTERVAL = (0, 5)
 PLOT_Y_TICKS_STEP = 0.1
 
 def get_vectors_df(filename=INPUT_FILENAME):
@@ -26,7 +23,7 @@ def main():
     dfdms = dfdms[dfdms.index.get_level_values(0) != dfdms.index.get_level_values(1)]
     print(f'Максимальное расстояние {float(dfdms.max())} у векторов {dfdms.idxmax()}')
     print(f'Минимальное расстояние {float(dfdms.min())} у векторов {dfdms.idxmin()}')
-    plot = dfdms[slice(*PLOT_X_INTERVAL)].plot(
+    dfdms[slice(*PLOT_X_INTERVAL)].plot(
         kind = "bar",
         yticks=np.arange(*PLOT_Y_TICKS_INTERVAL, PLOT_Y_TICKS_STEP),
         figsize=(25,10)
